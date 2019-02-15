d87b5cb66407455b78404247819099bbff9225b1
GingerPowder <jamesw@buffalo.edu>
2019-02-14 21:18:15 -0500

    i love comp sci

--- src/workpls/youmom.scala
+++ src/workpls/youmom.scala
@@ -3,5 +3,6 @@ package workpls
 object youmom {
   def main(args: Array[String]): Unit = {
      println("youmom dfansiojkfdsbakjfdsbajdsbjdsb kjgf skjgfdb kjgfnsd  ")
+      println("no u")
   }
 }
